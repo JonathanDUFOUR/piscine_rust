5bbfc83dd84c6a67e50bc74790af41df6ad17c7f
jodufour <jodufour@student.42.fr>
2023-08-23 08:59:48 +0200

    Module 03 [DOING]   ex02    [OK]   ex03    [TODO]

--- module_03/ex02/src/main.rs
+++ module_03/ex02/src/main.rs
@@ -1,3 +1,45 @@
+struct John;
+
+use std::fmt;
+impl std::fmt::Display for John {
+	fn fmt(self: &Self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		if f.precision() == Some(0) {
+			write!(f, "Don't try to silence me!")
+		} else {
+			f.pad("Hey! I'm John.")
+		}
+	}
+}
+
+impl std::fmt::Debug for John {
+	fn fmt(self: &Self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		write!(
+			f,
+			"John, the man himself.{}",
+			if f.alternate() {
+				" He's awesome AND formidable."
+			} else {
+				""
+			}
+		)
+	}
+}
+
+impl std::fmt::Binary for John {
+	fn fmt(self: &Self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		write!(f, "Bip Boop?")
+	}
+}
+
 fn main() {
-	println!("Hello, world!");
+	let john = John;
+
+	println!("1. {john}");
+	println!("2. |{john:<30}|");
+	println!("3. |{john:>30}|");
+	println!("4. {john:.6}");
+	println!("5. {john:.0}");
+	println!("6. {john:?}");
+	println!("7. {john:#?}");
+	println!("8. {john:b}");
 }
