904931da3122271f1392badc635056af8d76a3d5
jodufour <jodufour@student.42.fr>
2023-08-26 17:13:42 +0200

    Module 03 [DOING]   ex04    [TEST]

--- module_03/ex04/src/main.rs
+++ module_03/ex04/src/main.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use std::fmt::Debug;
 use std::fmt::Display;
 use std::fmt::Formatter;
 
@@ -6,6 +7,20 @@ enum TimeParseError {
 	MissingColon,
 	InvalidLength,
 	InvalidNumber,
+}
+
+impl Debug for TimeParseError {
+	fn fmt(self: &Self, formatter: &mut Formatter<'_>) -> fmt::Result {
+		write!(
+			formatter,
+			"{}",
+			match self {
+				Self::MissingColon => "missing ':'",
+				Self::InvalidLength => "invalid length",
+				Self::InvalidNumber => "invalid number",
+			}
+		)
+	}
 }
 
 impl Display for TimeParseError {
@@ -32,14 +47,54 @@ impl std::str::FromStr for Time {
 
 	fn from_str(s: &str) -> Result<Self, Self::Err> {
 		let s_bytes: &[u8] = s.as_bytes();
+		let colon_index = {
+			let mut i: usize = 0;
+			while i < s_bytes.len() {
+				if s_bytes[i] == b':' {
+					break;
+				}
+				i += 1;
+			}
+			i
+		};
 
-		if s_bytes.len() != 5 {
+		if colon_index == s_bytes.len() {
+			return Err(Self::Err::MissingColon);
+		}
+		if s_bytes[..colon_index].len() > 2 || s_bytes[colon_index + 1..].len() != 2 {
 			return Err(Self::Err::InvalidLength);
 		}
-		// TODO: Split on colon, checking if it is missing at the same time.
-		// TODO: Check if there is only 2 resulting strings.
-		// TODO: Check if the resulting strings have both a length of 2.
-		// TODO: Parse both resulting strings as u32, and check the numbers are valid as hours and minutes.
+		for i in 0..colon_index {
+			if !s_bytes[i].is_ascii_digit() {
+				return Err(Self::Err::InvalidNumber);
+			}
+		}
+		for i in colon_index + 1..s.len() {
+			if !s_bytes[i].is_ascii_digit() {
+				return Err(Self::Err::InvalidNumber);
+			}
+		}
+
+		let hours: u32 = match s[..colon_index].parse() {
+			Ok(ok) => ok,
+			_ => return Err(Self::Err::InvalidNumber),
+		};
+		let minutes: u32 = match s[colon_index + 1..].parse() {
+			Ok(ok) => ok,
+			_ => return Err(Self::Err::InvalidNumber),
+		};
+
+		if hours > 23 || minutes > 59 {
+			return Err(Self::Err::InvalidNumber);
+		}
+
+		Ok(Self { hours, minutes })
+	}
+}
+
+impl Debug for Time {
+	fn fmt(self: &Self, formatter: &mut Formatter<'_>) -> fmt::Result {
+		write!(formatter, "{} hours, {} minutes", self.hours, self.minutes)
 	}
 }
 
@@ -55,6 +110,10 @@ fn main() {
 
 	println!("{a}");
 	println!("{b}");
+
+	for c in "Hello World".as_bytes() {
+		println!("{}", c);
+	}
 
 	let err1: TimeParseError = "12.20".parse::<Time>().unwrap_err();
 	let err2: TimeParseError = "12:2".parse::<Time>().unwrap_err();
