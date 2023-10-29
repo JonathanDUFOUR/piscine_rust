9708757c85f2d3cb96a7e82a6523893d0536ff07
jodufour <jodufour@student.42.fr>
2023-10-29 06:41:04 +0100

    Module 04 [DOING]   ex04    [DOING]

--- module_04/ex04/src/lib.rs
+++ module_04/ex04/src/lib.rs
@@ -1,13 +1,17 @@
+use std::collections::BTreeSet;
+
+const SIEVE_COUNT: usize = 42;
+
 pub struct Prime {
 	n: u32,
-	unit: u8,
 	primes: Vec<u32>,
+	sieve: BTreeSet<u32>, // See https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
 }
 
 impl Prime {
 	/// Creates a new Prime iterator instance and initializes its attributes.
-	/// The newly created Prime iterator instance will generate every prime number
-	/// that fits a 32-bits unsigned integer starting at a given number.
+	/// The newly created Prime iterator instance is used to get the prime numbers
+	/// starting at `n`, generating the next one at each iteration.
 	///
 	/// # Paramters
 	/// * `n` - The starting number.
@@ -19,10 +23,47 @@ impl Prime {
 	/// ```
 	/// use ex04::Prime;
 	///
-	/// let mut prime: Prime = Prime::new();
+	/// let mut prime: Prime = Prime::new(0);
 	/// ```
 	pub fn new(n: u32) -> Prime {
-		Prime { n, unit: 7, primes: vec![2, 3, 5, 7] }
+		const MAX_SIEVE_COUNT: usize = u32::MAX as usize;
+
+		match SIEVE_COUNT {
+			1..=MAX_SIEVE_COUNT => (),
+			_ => panic!("SIEVE_COUNT must be in [1, {MAX_SIEVE_COUNT}]."),
+		}
+
+		Prime { n, primes: vec![2, 3, 5, 7], sieve: BTreeSet::new() }
+	}
+
+	/// Fills the sieve with the next chunk of numbers.
+	/// The next chunk of numbers is composed by the `SIEVE_COUNT` numbers that directly
+	/// follow the last number of `self.primes`.
+	///
+	/// # Return
+	/// * `Some(())` - The sieve has been filled with the next chunk of numbers.
+	/// * `None` - There is no next chunk of numbers to fill the sieve with.
+	fn fill_sieve_with_next_chunk(self: &mut Self) -> Option<()> {
+		const SIEVE_COUNT_U32: u32 = SIEVE_COUNT as u32;
+
+		let primes_last: u32 = *self.primes.last().unwrap();
+		let remaining_numbers_count: u32 = u32::MAX - primes_last;
+		let chunk_count: usize = match remaining_numbers_count {
+			0 => return None,
+			1..=SIEVE_COUNT_U32 => remaining_numbers_count as usize,
+			_ => SIEVE_COUNT,
+		};
+
+		self.sieve.extend(primes_last..primes_last + chunk_count as u32);
+
+		Some(())
+	}
+
+	/// Removes the non-prime numbers from the sieve.
+	/// The non-prime numbers are found by removing the multiples of the prime numbers
+	/// that are already in `self.primes`.
+	fn remove_non_prime_from_sieve(self: &mut Self) {
+		// TODO
 	}
 }
 
@@ -32,13 +73,14 @@ impl Iterator for Prime {
 	/// Generates the next prime number.
 	///
 	/// # Return
-	/// The next prime number.
+	/// * `Some(n)` - The next prime number.
+	/// * `None` - There is no next prime number.
 	///
 	/// # Example
 	/// ```
 	/// use ex04::Prime;
 	///
-	/// let mut prime: Prime = Prime::new();
+	/// let mut prime: Prime = Prime::new(0);
 	///
 	/// assert_eq!(prime.next(), Some(2));
 	/// assert_eq!(prime.next(), Some(3));
@@ -48,49 +90,42 @@ impl Iterator for Prime {
 	/// ```
 	fn next(self: &mut Self) -> Option<Self::Item> {
 		let next_prime: u32;
-		let last_prime: u32 = *self.primes.last().unwrap();
 
-		if self.n < last_prime {
-			next_prime = lower_bound(&self.primes, self.n);
-			self.n = upper_bound(&self.primes, self.n);
-			Some(next_prime)
-		} else if self.n == last_prime {
-			// TODO: Give `next_prime` the value of the last element of `self.primes`.
-			// Then, append the next prime number to `self.primes` only once,
-			// and give `self.n` its value.
-			None
+		if self.n < *self.primes.last().unwrap() {
+			next_prime = self.primes[lower_bound(&self.primes, self.n).unwrap()];
+			self.n = self.primes[upper_bound(&self.primes, self.n).unwrap()];
 		} else {
-			// TODO: Append the next prime numbers to `self.primes`
-			// until the last element of `self.primes` is greater or equal to `self.n`.
-			// Once done, give `next_prime` the value of the last element of `self.primes`.
-			// Then, append the next prime number to `self.primes` only once,
+			while self.n > *self.primes.last().unwrap() {
+				if self.sieve.is_empty() {
+					self.fill_sieve_with_next_chunk()?;
+					self.remove_non_prime_from_sieve();
+				}
+
+				self.primes.push(self.sieve.pop_first().unwrap());
+			}
+
+			next_prime = *self.primes.last().unwrap();
+			// TODO: Find and append the next prime number to `self.primes` only once,
 			// and give `self.n` its value.
-			// Finally, return `Some(next_prime)`.
-			None
 		}
+
+		Some(next_prime)
 	}
 }
 
 /// Searches in `v` for the first element that is __greater or equal__ to `n`.
-/// It is assumed that `v` is sorted in ascending order,
-/// and that `n` is lower or equal to the last element of `v`.
+/// It is assumed that `v` is sorted in ascending order.
 ///
 /// # Parameters
 /// * `v` - The vector to search in.
-/// * `n` - The value to search the lower bound for.
+/// * `n` - The number to search the lower bound for.
 ///
 /// # Return
-/// The value of the first element in `v` that is greater or equal to `n`.
-///
-/// # Panic
-/// * `v` is empty.
-/// * `n` is strictly greater than the last element of `v`.
-fn lower_bound(v: &Vec<u32>, n: u32) -> u32 {
-	if v.is_empty() {
-		panic!("The given vector is empty.");
-	}
-	if *v.last().unwrap() < n {
-		panic!("{n} is greater than the last element of the given vector.");
+/// * `Some(i)` - The index of the first element in `v` that is greater or equal to `n`.
+/// * `None` - There is no element in `v` that is greater or equal to `n`.
+fn lower_bound(v: &Vec<u32>, n: u32) -> Option<usize> {
+	if v.is_empty() || *v.last().unwrap() < n {
+		return None;
 	}
 
 	let mut left: usize = 0;
@@ -106,48 +141,37 @@ fn lower_bound(v: &Vec<u32>, n: u32) -> u32 {
 		}
 	}
 
-	v[left]
+	Some(left)
 }
 
-/// Searches in `v` for the first element that is strictly greater__ than `n`.
-/// It is assumed that `v` is sorted in ascending order,
-/// and that `n` is strictly lower than the last element of `v`.
+/// Searches in `v` for the first element that is __strictly greater__ than `n`.
+/// It is assumed that `v` is sorted in ascending order.
 ///
 /// # Parameters
 /// * `v` - The vector to search in.
-/// * `n` - The value to search the lower bound for.
+/// * `n` - The number to search the upper bound for.
 ///
 /// # Return
-/// The value of the first element in `v` that is strictly greater than `n`.
-///
-/// # Panic
-/// * `v` is empty.
-/// * `n` is greater or equal to the last element of `v`.
-fn upper_bound(v: &Vec<u32>, n: u32) -> u32 {
-	if v.is_empty() {
-		panic!("The given vector is empty.");
-	}
-	if *v.last().unwrap() <= n {
-		panic!("{n} is greater than the last element of the given vector.");
-	}
-
-	let mut left: usize = 0;
-	let mut right: usize = v.len();
-
-	while left < right {
-		let middle: usize = left + (right - left) / 2;
-
-		if v[middle] <= n {
-			left = middle + 1;
-		} else {
-			right = middle;
+/// * `Some(i)` - The index of the first element in `v` that is strictly greater than `n`.
+/// * `None` - There is no element in `v` that is strictly greater than `n`.
+fn upper_bound(v: &Vec<u32>, n: u32) -> Option<usize> {
+	match lower_bound(v, n) {
+		Some(i) => {
+			if v[i] == n {
+				if i + 1 < v.len() {
+					Some(i + 1)
+				} else {
+					None
+				}
+			} else {
+				Some(i)
+			}
 		}
+		None => None,
 	}
-
-	v[left]
 }
 
 #[cfg(test)]
 mod tests {
-	use super::*;
+	// use super::*;
 }
